432d75178852de266a48e7b2e6535c2961783aa1
Dizzy <ches@ukr.net>
2015-11-16 16:15:17 +0200

    Added delete and save methods

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -41,4 +41,66 @@ class UserController extends CController
         ));
     }
 
+    /**
+     * Creates a new user or updates an existing user
+     * @param  int $id
+     */
+    public function actionSave($id = null)
+    {
+        $user = ($id == null) ? new User() : $this->loadModel($id);
+
+        if (isset($_POST['User']))
+        {
+            $user->attributes = $_POST['User'];
+
+            try {
+                if ($user->save())
+                {
+                    Yii::app()->user->setFlash('success', 'The user has sucessfully been updated');
+                    $this->redirect($this->createUrl('user/save', array('id' => $user->id)));
+                }
+            } catch (Exception $e) {
+                $user->addError('email', 'A user with that email address already exists');
+            }
+        }
+
+        $this->render('save', array(
+            'model' => $user
+        ));
+    }
+
+    /**
+     * Delets a particular user
+     * @param  int $id
+     */
+    public function actionDelete($id = null)
+    {
+        if (Yii::app()->user->id == $id)
+            throw new CHttpException(403, 'You can not delete yourself');
+
+        $user = $this->loadModel($id);
+
+        if ($user->delete())
+            $this->redirect($this->createUrl('user/'));
+
+        throw new CHttpException(400, 'Bad Request');
+    }
+
+    /**
+     * Loads a model with a given ID, and throws the appropriate error
+     * @param  int $id
+     * @return User::model
+     */
+    private function loadModel($id = null)
+    {
+        if (id == null)
+            throw new CHttpException(400, 'Missing ID argument');
+
+        $model = User::model()->findByPk($id);
+
+        if ($model == null)
+            throw new CHttpException(404, 'No user with that ID could be found');
+
+        return $model;
+    }
 }
