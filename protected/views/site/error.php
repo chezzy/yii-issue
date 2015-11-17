ea9c7ef935c2f96e599d5e94a07e9bcf41e950a6
Dizzy <ches@ukr.net>
2015-11-17 17:13:48 +0200

    Added site layouts

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,15 +1,4 @@
-<?php
-/* @var $this SiteController */
-/* @var $error array */
-
-$this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
-?>
-
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+<div class="jumbotron">
+	<h1>Error <?php echo $error['code']; ?></h1>
+	<p><?php echo $error['message']; ?></p>
 </div>
